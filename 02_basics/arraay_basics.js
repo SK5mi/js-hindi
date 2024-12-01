cc048c9e42eca747dc65aab34397efff13de530a
Satyam Mishra <satyamkumarmishra071@gmail.com>
2024-12-01 18:05:00 +0000

    array

---
+++ 02_basics/arraay_basics.js
@@ -0,0 +1,22 @@
+const numbers = [1,2,3,4,5,6,7,8,9];
+/*console.log(numbers.pop())
+console.log(numbers.push(65));
+console.log(numbers);
+console.log(numbers.includes(5));*/
+// console.log(numbers.at[6])
+// console.log(numbers.unshift(68,55));
+// console.log(28,numbers);
+// console.log(console.log(numbers.shift(8)));
+// console.log(numbers);
+// console.log(forEach[+10]);
+
+// console.log(numbers.forEach(2))
+// numbers.forEach((num, index) => {
+//   console.log(`${num}* ${index}= ${num * index}`)
+//numbers[index] =  });
+
+numbers.forEach((num, index) => {
+  numbers[index]=(num * 2);
+});
+
+console.log(numbers);
