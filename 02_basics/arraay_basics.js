de9f5c6fbd98ba1501606006267d172966e2e1dc
Satyam Mishra <satyamkumarmishra071@gmail.com>
2024-12-01 19:10:38 +0000

    array

--- 02_basics/arraay_basics.js
+++ 02_basics/arraay_basics.js
@@ -1,4 +1,4 @@
-const numbers = [1,2,3,4,5,6,7,8,9];
+const numbers = [1,2555,3,4,5,6,7,8,9];
 /*console.log(numbers.pop())
 console.log(numbers.push(65));
 console.log(numbers);
@@ -13,10 +13,25 @@ console.log(numbers.includes(5));*/
 // console.log(numbers.forEach(2))
 // numbers.forEach((num, index) => {
 //   console.log(`${num}* ${index}= ${num * index}`)
-//numbers[index] =  });
+// //numbers[index] =  });
 
-numbers.forEach((num, index) => {
-  numbers[index]=(num * 2);
-});
+// numbers.forEach((num, index) => {
+//   numbers[index]=(num * 2);
+// });
 
-console.log(numbers);
+// console.log(numbers);
+
+// console.log(numbers.includes(5));
+// console.log(numbers);
+// console.log(numbers.sort());
+// console.log(numbers.reverse());
+console.log(typeof(numbers))
+const addition = [11,22,33,44,55,66,77,88,99,110,121,133]
+
+// const jod = numbers.concat(addition);
+// // console.log(jod);s
+// console.log(...numbers,...addition);
+
+console.log(numbers.join('*'));
+
+console.log(typeof(numbers))
