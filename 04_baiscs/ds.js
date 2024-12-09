7cbd3c907cf6f5cea9d14550623b021126bd9944
Satyam Mishra <satyamkumarmishra071@gmail.com>
2024-12-09 15:47:31 +0000

    destructure

---
+++ 04_baiscs/ds.js
@@ -0,0 +1,24 @@
+/*const family ={
+    grand_father : "ramkanthe",
+    grand_mother :'syama',
+    papa :"s.p. mishra",
+    son :"dstendra",
+}
+const {son} = family;   
+
+const {son : beta} = family;   // now we can call son also by the name of beta
+console.log(beta);
+console.log(son);
+*/
+// when data is inside of these these curly bracket without having any name so that data is json data
+//  .in json all variable is in string format means inside double inveerted comma
+
+// {
+//     {
+//      "name" :"paan singh tomar"                   
+//     },
+//     {
+//       "age" :66,
+//       "is_varified" :true
+//     }
+// }
