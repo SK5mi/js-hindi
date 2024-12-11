9472135b2b14dd0f15e7d1277f393e5da8085ebc
Satyam Mishra <satyamkumarmishra071@gmail.com>
2024-12-11 12:52:50 +0000

    function

---
+++ 05_object/function.js
@@ -0,0 +1,28 @@
+// function without return variable
+/*function AddTwoNumber(a,b){
+console.log(a + b);
+}
+const ans = AddTwoNumber(8,8);
+console.log(ans);
+
+*/
+
+
+// functio with  return 
+
+/*
+function AddTwoNumber(a,b){
+   const Ans = a + b;
+   return Ans
+    }
+    const ans = AddTwoNumber(8,8);
+    console.log(ans);
+
+*/
+
+
+function AddTwoNumber(a,b){
+    return ( a + b);
+}
+ console.log(AddTwoNumber(8,8));
+ 
